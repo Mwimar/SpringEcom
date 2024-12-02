496985dcd68e91df2096812823758bee5f2e2927
mwimar <markmwimar@gmail.com>
2024-12-03 00:38:34 +0300

    product controller

--- src/main/java/com/mwimar/SpringEcom/controllers/HelloController.java
+++ src/main/java/com/mwimar/SpringEcom/controllers/HelloController.java
@@ -1,5 +1,12 @@
 package com.mwimar.SpringEcom.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HelloController {
+    @GetMapping("/hello")
+    public String greet(){
+        return "Welcome to Mwimar";
+    }
 }
--- src/main/java/com/mwimar/SpringEcom/controllers/ProductController.java
+++ src/main/java/com/mwimar/SpringEcom/controllers/ProductController.java
@@ -1,9 +1,15 @@
 package com.mwimar.SpringEcom.controllers;
 
-@RestController("api")
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class ProductController {
-    @RequestMapping("/products")
-    public String getAllProducts(){
-        return "AllProducts";
+    @GetMapping("/products")
+public String getProducts(){
+        return "All Products";
     }
 }
